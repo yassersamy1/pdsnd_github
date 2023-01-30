544a6c0d8fd1dbffd167997d2b7653edd0f595f1
yassersamy1 <yasseralali10@gmail.com>
2023-01-30 22:36:58 +0300

    added closing print statement

--- bikeshare.py
+++ bikeshare.py
@@ -189,6 +189,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you, and have a nice day")
             break
 
 
